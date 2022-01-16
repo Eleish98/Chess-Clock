4dc59c44af72769f84f800a3863db67ab51687d8
Eleish <mohammeshikh@yahoo.com>
2022-01-16 23:53:17 +0200

    Added Private Variables for Chess Clock Application

--- Chess Clock/App/ChessClock_private.h
+++ Chess Clock/App/ChessClock_private.h
@@ -8,6 +8,39 @@
 #ifndef CHESSCLOCK_PRIVATE_H_
 #define CHESSCLOCK_PRIVATE_H_
 
+typedef enum {
+
+	STATE_STOPPED,
+	STATE_SAVED_MODE_SELECTED,
+	STATE_CONFIGURE_TIME_CONTROL,
+	STATE_STARTING, STATE_PLAYING
+}ChessClockState_t;
+
+typedef enum {
+
+	CHESS_PLAYER_1,
+	CHESS_PLAYER_2
+}ChessPlayerTag_t;
+
+typedef enum {
+
+	TURN_PLAYER_1,
+	TURN_PLAYER_2
+}ChessTurn_t;
+
+typedef struct {
+
+	ChessPlayerTag_t Tag;
+	u16 Time;
+}ChessPlayer_t;
+
+
+
+ChessClockState_t PCHESS_CLOCK_STATE;
+
+ChessPlayer_t ChessPlayer1, ChessPlayer2;
+
+
 
 
 #endif /* CHESSCLOCK_PRIVATE_H_ */
--- Chess Clock/App/ChessClock_program.c
+++ Chess Clock/App/ChessClock_program.c
@@ -4,21 +4,22 @@
  *  Created on: Jan 16, 2022
  *      Author: 20109
  */
-
+//Library Files Includes
 #include "1- LIB/STD_TYPES.h"
 #include "1- LIB/BIT_MATH.h"
 
+//Core Peripherals Includes
 #include "2- MCAL/ARM _CORTEX_M3/01- NVIC/NVIC_interface.h"
 #include "2- MCAL/ARM _CORTEX_M3/03- STK/STK_interface.h"
 
-
+//MCAL Peripherals includes
 #include "2- MCAL/ST/04- RCC/RCC_interface.h"
 #include "2- MCAL/ST/05- GPIO/GPIO_interface.h"
 #include "2- MCAL/ST/06- AFIO/AFIO_interface.h"
 #include "2- MCAL/ST/07- EXTI/EXTI_interface.h"
 
-
-
+//HAL Includes
+#include "3- HAL/03- Char LCD/LCD_interface.h"
 
 void ChessClock_voidInit(void){
 
