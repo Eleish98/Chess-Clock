b1482c5cfef123efb52f8f7c86648ff0b683396b
Eleish <mohammeshikh@yahoo.com>
2022-01-17 23:09:57 +0200

    Added Rotary Encoder Driver (Not Tested)

---
+++ Chess Clock/Drivers/3- HAL/04- Rotary Encoder/Encoder_config.h
@@ -0,0 +1,21 @@
+/*
+ * Encoder_config.h
+ *
+ *  Created on: Jan 17, 2022
+ *      Author: 20109
+ */
+
+#ifndef ROTARY_ENCODER_ENCODER_CONFIG_H_
+#define ROTARY_ENCODER_ENCODER_CONFIG_H_
+
+#define ENCODER_CHANNEL_A_PORT			GPIO_PORTA
+
+#define ENCODER_CHANNEL_A_PIN			GPIO_PIN_0
+
+#define ENCODER_CHANNEL_B_PORT			GPIO_PORTA
+
+#define ENCODER_CHANNEL_B_PIN			GPIO_PIN_1
+
+
+
+#endif /* 3__HAL_04__ROTARY_ENCODER_ENCODER_CONFIG_H_ */
